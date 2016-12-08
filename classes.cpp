eee477fbe9be6a387555614969cb7113ea2cc64a
Paulo Valente <polvalente@gmail.com>
2016-12-08 20:29:31 +0000

    Pulse font implemented

--- classes.cpp
+++ classes.cpp
@@ -15,6 +15,32 @@ double Elemento::valorFonteSenoidal(double t){
 }
 
 double Elemento::valorFontePulse(double t, double passo){
+	if (t < atraso)
+		return amplitude;
+	double tempo = fmod(t-atraso, periodo);
+	tempoDescida = (tempoDescida == 0) ? passo : tempoDescida;
+  tempoSubida = (tempoSubida == 0) ? passo : tempoSubida;
+
+	if (tempo < tempoSubida){
+		double a = (amplitude2 - amplitude)/tempoSubida;
+		double b = amplitude;
+
+		return a * tempo + b;
+	}
+
+	if (tempo < tempoSubida + tempoLigada){
+		return amplitude2;
+	}
+
+	if (tempo < tempoSubida + tempoLigada + tempoDescida){
+		double a = (amplitude - amplitude2)/tempoDescida;
+		double b = amplitude2 - a * (tempoSubida + tempoLigada);
+		return a * tempo + b;
+	}
+
+	return amplitude;
+
+	/*
 	double tempo = fmod(t,periodo);
 	if (tempoDescida == 0){
 		tempoDescida = passo;
@@ -37,5 +63,5 @@ double Elemento::valorFontePulse(double t, double passo){
 	else{
 	//decaindo de amp2 a amp1
 		return amplitude2 + (amplitude - amplitude2)/tempoDescida * (tempo - (atraso + tempoSubida+ tempoLigada));
-	}
+	}*/
 }
