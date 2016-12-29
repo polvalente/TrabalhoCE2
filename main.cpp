97681c30e98166f0c919a52a6a1d943ce11e6599
Paulo Valente <polvalente@gmail.com>
2016-12-28 23:22:28 -0200

    Adding comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
-/* Trabalho de Circuitos Eletricos II
+/* Electrical Circuits Simulator - work for class EEL525 at DEL/UFRJ
+ *
+ * Based on MNA1 by Antonio Carlos Moreirao de Queiroz - acmq@coe.ufrj.br
  * Alice Fontes - alicefontes@poli.ufrj.br
  * Camyla Tsukuda Romao - camyla.romao@poli.ufrj.br
  * Paulo Oliveira Lenzi Valente - paulovalente@poli.ufrj.br
- * 
- * Baseado no programa mna1 (por Antonio Carlos M. de Queiroz - acmq@coe.ufrj.br)
  *
- * O simulador aceita os seguintes tipos de elementos no netlist:
- * Resistor:  R<nome> <no+> <no-> <resistencia>
- * VCCS:      G<nome> <io+> <io-> <vi+> <vi-> <transcondutancia>
- * VCVC:      E<nome> <vo+> <vo-> <vi+> <vi-> <ganho de tensao>
- * CCCS:      F<nome> <io+> <io-> <ii+> <ii-> <ganho de corrente>
- * CCVS:      H<nome> <vo+> <vo-> <ii+> <ii-> <transresistencia>
- * Fonte I:   I<nome> <io+> <io-> <corrente>
- * Fonte V:   V<nome> <vo+> <vo-> <tensao>
- * Amp. op.:  O<nome> <vo1> <vo2> <vi1> <vi2>
- * 
- * As fontes F e H tem o ramo de entrada em curto
- * O amplificador operacional ideal tem a saida suspensa
- * Os nos podem ser nomes
+ * The following components can be in the netlist:
  *
+ * Comment     - *<comment>
+ *
+ * Resistor    -  R<name> <node 1> <node 2> <value>
+ * Capacitor   -  C<name> <node 1> <node 2> <value>
+ * Inductor    -  L<name> <node 1> <node 2> <value>
+ *
+ * Transformer -  K<name> <node 1+> <node 1-> <node 2+> <node 2-> <n>
+ *
+ * Op.Amp.     -  O<name> <node out+> <node out-> <node in+> <node in->
+ *
+ * V.C.V.S.    -  E<name> <node out+> <node out-> <node in+> <node in-> <Gain> 
+ * C.C.C.S.    -  F<name> <node out+> <node out-> <node in+> <node in-> <Gain>
+ * V.C.C.S.    -  G<name> <node out+> <node out-> <node in+> <node in-> <Gain> 
+ * C.C.V.S.    -  H<name> <node out+> <node out-> <node in+> <node in-> <Gain>
+ *
+ * Diode       -  D<name> <node +> <node ->
+ * Switch      -  $<name> <node +> <node -> <control node +> <control node -> <limit voltage>
  * */
 
 #include <iostream>
@@ -50,20 +55,16 @@ int main(int argc, char** argv)
 	cout << "Baseado no programa MNA1 - por Antonio Carlos M. de Queiroz - acmq@coe.ufrj.br\n"<< endl;
   cout << "Versao " << versao << "\n\n" << endl;
 
-  /* Leitura do netlist */
+  /* Reading the netlist - can take optional command line argument as filename*/
 	string nomeArquivo = "";
 	if (argc == 2)
 		nomeArquivo = argv[1];
 	leituraNetlist(lista, netlist, componentesVariantes, argc, nomeArquivo, num_variaveis, tempo_final, passo, metodo, passos_por_ponto, componentesNaoLineares);
 	if (componentesVariantes.size() > 0) metodo = "TRAP";
 
-  /* Acrescenta variaveis de corrente acima dos nos, anotando no netlist */
+	/* Creating the variables for the currents that need to be calculated*/
 	adicionarVariaveis(lista, netlist, num_variaveis, num_nos);
-	//cin.get();
-
-  
 	
-	vector<vector<long double>> Yn(num_variaveis+1, vector<long double>(num_variaveis+2));
 #ifdef DEBUG
 	cout << "Metodo de simulacao: " << metodo << endl;
 #endif
@@ -74,6 +75,8 @@ int main(int argc, char** argv)
 	}
 	else{ /* DC */
 		/* Lista tudo */
+		vector<vector<long double>> Yn(num_variaveis+1, vector<long double>(num_variaveis+2));
+		
 		listarVariaveis(lista, num_variaveis);
 		cin.get();
 
